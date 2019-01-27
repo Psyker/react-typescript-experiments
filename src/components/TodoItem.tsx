fd65965144b418644bd4bf27ac8a70167fd11ec1
Psyker <bourgoin.theo1@gmail.com>
2019-01-27 23:34:22 +0100

    Add edition of todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
-import {ChangeEventHandler, FunctionComponent, useRef} from "react";
+import {ChangeEventHandler, FunctionComponent,  useState} from "react";
 import styled from "styled-components";
 import appStore, {Todo} from "../store/AppStore";
 import {observer} from "mobx-react-lite";
+import {useInput} from "../utils/hooks";
 
 interface Props {
     readonly todo: Todo
@@ -11,19 +12,35 @@ interface Props {
 const TodoItem: FunctionComponent<Props> = (props) => {
     const {todo} = props;
     const {changeTodo} = appStore;
-    const input = useRef<HTMLInputElement>(null);
+    const {setValue, ...todoInput}= useInput(todo.name);
+    const [isEditing, setIsEditing] = useState(false);
 
     const onChangeHandler: ChangeEventHandler<HTMLInputElement> = event => {
         changeTodo({...todo, done: event.target.checked})
     };
 
+    const onDoubleClickHandler: React.MouseEventHandler<HTMLInputElement> = () => {
+        setIsEditing(true)
+    };
+
+    const onBlurHandler: React.FocusEventHandler<HTMLInputElement> = () => {
+        setIsEditing(false);
+        if (todoInput.value != '') {
+            changeTodo({...todo, name: todoInput.value, updatedAt: new Date()});
+            setValue('')
+        }
+    };
+
     return (
         <div {...props}>
-            <input ref={input} onChange={onChangeHandler} type="checkbox"/> {todo.name}
+            <input onChange={onChangeHandler} type="checkbox"/>
+            {
+                isEditing
+                ? <input {...todoInput} onBlur={onBlurHandler}/>
+                : <span onDoubleClick={onDoubleClickHandler}>{todo.name}</span>
+            }
         </div>
     );
 };
 
-export default styled(observer(TodoItem))`
-
-`
+export default styled(observer(TodoItem))``
